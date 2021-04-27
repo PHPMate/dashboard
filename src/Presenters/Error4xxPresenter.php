1e88ca837783f34552aaa29dd656d55553daa1e1
Jan Mikeš <j.mikes@me.com>
2021-04-27 23:51:13 +0200

    Fixed phpstan errors

--- src/Presenters/Error4xxPresenter.php
+++ src/Presenters/Error4xxPresenter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PHPMate\Dashboard\Presenters;
 
 use Nette;
+use Nette\Application\UI\Template;
 
 
 final class Error4xxPresenter extends Nette\Application\UI\Presenter
@@ -12,7 +13,7 @@ final class Error4xxPresenter extends Nette\Application\UI\Presenter
 	public function startup(): void
 	{
 		parent::startup();
-		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
+		if (!$this->getRequest()?->isMethod(Nette\Application\Request::FORWARD)) {
 			$this->error();
 		}
 	}
@@ -22,6 +23,9 @@ final class Error4xxPresenter extends Nette\Application\UI\Presenter
 	{
 		// load template 403.latte or 404.latte or ... 4xx.latte
 		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
-		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
+
+		/** @var Template $template */
+		$template = $this->template;
+		$template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
 	}
 }
