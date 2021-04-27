1a2897f2687aa2e65415d4abf64972b7f9bf832b
Jan Mikeš <j.mikes@me.com>
2021-04-27 23:55:17 +0200

    Tabs to spaces

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -9,21 +9,21 @@ use Nette\Bootstrap\Configurator;
 
 class Bootstrap
 {
-	public static function boot(): Configurator
-	{
-		$configurator = new Configurator;
+    public static function boot(): Configurator
+    {
+        $configurator = new Configurator;
 
         $configurator->enableTracy(__DIR__ . '/../var/log');
 
-		if (isset($_ENV['APP_DEBUG']) && (bool) $_ENV['APP_DEBUG'] === true) {
-		    $configurator->setDebugMode(true);
+        if (isset($_ENV['APP_DEBUG']) && (bool) $_ENV['APP_DEBUG'] === true) {
+            $configurator->setDebugMode(true);
         }
 
-		$configurator->setTimeZone('Europe/Prague');
-		$configurator->setTempDirectory(__DIR__ . '/../var/cache');
+        $configurator->setTimeZone('Europe/Prague');
+        $configurator->setTempDirectory(__DIR__ . '/../var/cache');
 
-		$configurator->addConfig(__DIR__ . '/../config/common.neon');
+        $configurator->addConfig(__DIR__ . '/../config/common.neon');
 
-		return $configurator;
-	}
+        return $configurator;
+    }
 }
